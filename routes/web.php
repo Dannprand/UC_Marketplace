40dbb54ddd85c1ac6b7edc261724c01e519f68b8
PandaJack69 <kevinartan2005@gmail.com>
2025-05-04 19:21:23 +0700

    added seperate folders for users and merchants

--- routes/web.php
+++ routes/web.php
@@ -2,30 +2,43 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Public Routes
 Route::get('/', function () {
     return view('landingPage');
 });
 
-Route::get('/home', function () {
-    return view('home');
-})->name('home');
-
-Route::get('/product', function () {
-    return view('product');
-})->name('product');
-
-Route::get('/cart', function () {
-    return view('cart');
-})->name('cart');
-
-Route::get('/register', function () {
-    return view('register');
-})->name('register');
-
+// Authentication Routes
 Route::get('/login', function () {
-    return view('login');
+    return view('auth.login');
 })->name('login');
 
-Route::get('/payment', function () {
-    return view('payment');
-})->name('payment');
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
+
+// User Routes
+Route::prefix('user')->group(function () {
+    Route::get('/home', function () {
+        return view('user_view.home');
+    })->name('home');
+    
+    Route::get('/cart', function () {
+        return view('user_view.cart');
+    })->name('cart');
+    
+    Route::get('/product', function () {
+        return view('user_view.product');
+    })->name('product');
+    
+    Route::get('/payment', function () {
+        return view('user_view.payment');
+    })->name('payment');
+});
+
+// Merchant Routes
+Route::prefix('merchant')->group(function () {
+    // You can add merchant-specific routes here later
+    Route::get('/dashboard', function () {
+        return view('merchant_view.dashboard');
+    })->name('merchant.dashboard');
+});
