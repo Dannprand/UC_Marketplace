3988cf5dea4347258221687fa46674fae816b5a6
Dannprand <153193400+Dannprand@users.noreply.github.com>
2025-06-02 20:15:38 +0700

    change margin home

--- resources/views/user_view/home.blade.php
+++ resources/views/user_view/home.blade.php
@@ -333,7 +333,7 @@
 
     <div class="animate__animated animate__fadeIn">
         <div class="scroll-section">
-            <h2 class="section-title mt-12">Hot Deals</h2>
+            <h2 class="section-title mt-16">Hot Deals</h2>
             <div class="scroll-container mt-12" id="autoScrollContainer">
                 <div class="scroll-item">Item 1</div>
                 <div class="scroll-item">Item 2</div>
